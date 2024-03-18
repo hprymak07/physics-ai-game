1a5f2efb1b6de29548911042fa9ac838848693cb
r1ftyz <landonbriggs05@gmail.com>
2024-03-18 12:55:12 -0400

    fixed and made small improvement

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Player:
     def __init__(self, x, y):
         self.rect = pygame.Rect(x, y, 10, 10)
         self.floor = pygame.Rect(0, 370, w, 200)
+        self.endpt = pygame.Rect(1150, 90, 20, 20)
         self.objects_lvl_one = [
             pygame.Rect(100, 300, 100, 100),
             pygame.Rect(250, 220, 100, 25),
@@ -35,17 +36,18 @@ class Player:
         self.acc = 0
 
     def draw_lvl(self):
-        screen.fill("white") # background
-        pygame.draw.rect(screen, 'red', player.rect) # Player
-        pygame.draw.rect(screen, 'black', (0, 370, w, 200))
-        pygame.draw.rect(screen, 'forestgreen', (1150, 90, 20, 20)) # End of the lvl
+        screen.fill((225, 225, 225)) # background
+
         for obj in self.objects_lvl_one: # platforms 
             pygame.draw.rect(screen, 'black', obj)
-
+            
+        pygame.draw.rect(screen, 'forestgreen', (1150, 90, 20, 20)) # End of the lvl
+        pygame.draw.rect(screen, 'red', player.rect) # Player
+        pygame.draw.rect(screen, 'black', (0, 370, w, 200)) # floor
     def update(self, dt):
         keys = pygame.key.get_pressed()
         self.acc = self.gravity
-        list_in_use = self.objects_lvl_one + list(self.floor)
+        list_in_use = self.objects_lvl_one + [self.floor]
 
         for objects in list_in_use:
             if self.rect.colliderect(objects):
@@ -82,15 +84,15 @@ class Player:
         if player.rect.left <= 0:
            player.rect.x = 20
         if player.rect.right >= w:
-           player.rect.x = 1220
+           player.rect.x = w - self.rect.width
     
     def is_game_over(self):
-      if player.rect.colliderect(self.endpoint):
+      if player.rect.colliderect(self.endpt):
         player.rect.x = 30
         player.rect.y = 0
         print("Game Over")
       if player.rect.colliderect(self.floor):
-          print("Game Over")
+          print("floor")
 
 player = Player(30, h / 2)
 
@@ -100,6 +102,7 @@ while True:
             break
 
     player.draw_lvl()
+    player.is_game_over()
 
     pygame.display.flip()
     dt = pygame.time.Clock().tick(60) / 1000
